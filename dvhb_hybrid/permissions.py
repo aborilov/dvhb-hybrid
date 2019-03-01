b457df2041ca0782e5465498736a71b0413b7b5a
Aborilov Pavel <aborilov@gmail.com>
2019-03-01 10:51:16 +0300

    check API-KEY in cookies also

--- dvhb_hybrid/permissions.py
+++ dvhb_hybrid/permissions.py
@@ -9,7 +9,7 @@ from dvhb_hybrid.redis import redis_key
 
 
 def get_api_key(request):
-    return request.headers.get('API-KEY') or request.GET.get('api_key')
+    return request.headers.get('API-KEY') or request.GET.get('api_key') or request.cookies.get('API-KEY')
 
 
 async def get_session_data(request, sessions=None):
